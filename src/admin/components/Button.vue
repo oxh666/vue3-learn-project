9caadf04542000b4e2dc742562b8be156cc2ea2e
oxh666 <jojo5981@outlook.com>
2023-09-13 10:35:19 +0800

    feature day02 不锈钢button 实现02-初步实现指纹，并定时消除

--- src/admin/components/Button.vue
+++ src/admin/components/Button.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="button-warp">
+    <div class="fingerprint" v-for="finger in fingerPrints"
+    :style="{
+    left:finger.x+'px',
+    top:finger.y+'px',
+    }"
+    ></div>
     <div class="button"
          :class="{pressed:isClick}"
          @pointerdown="handlePoint"
@@ -18,9 +24,20 @@ export default defineComponent({
     const msg = ref<string>('同尘和光')
     const isClick = ref<boolean>(false)
     const container = ref(null)
-    
+    const fingerPrints=ref<any>([])
     function handlePoint(e) {
       isClick.value = true
+      const id =Math.random().toString(16).slice(2)
+      const btn=container.value.getBoundingClientRect()
+      const finger={
+        id,
+        x:e.clientX-btn.left,
+        y:e.clientY-btn.top
+      }
+      fingerPrints.value.push(finger)
+      setTimeout(()=>{
+        fingerPrints.value=fingerPrints.value.filter(f=>f.id!== id)
+      },2000)
     }
     
     onMounted(() => {
@@ -40,6 +57,7 @@ export default defineComponent({
       msg,
       isClick,
       container,
+      fingerPrints,
       handlePoint
     }
   },
@@ -48,13 +66,27 @@ export default defineComponent({
 </script>
 
 <style scoped lang="less">
+.fingerprint{
+  pointer-events: none;
+  z-index: 100;
+  position: absolute;
+  left:0;
+  top:0;
+  transform: translate(-50%,-50%);
+  width: 60px;
+  height: 60px;
+  background: rgba(225,225,225,0.4);
+  border-radius:  100%;
+  //mask-image: linear-gradient(50% 50% at 50% 50%, #00000 0%, rgba(0,0,0,0) 100%);
+  mask-image: linear-gradient(rgba(0, 0, 0, 1), transparent);
+}
 .button-warp {
   width: 300px;
   height: 100px;
   position: relative;
   overflow: hidden;
   border-radius: 60px;
-  
+
   .button {
     width: 100%;
     height: 100%;
@@ -62,6 +94,15 @@ export default defineComponent({
     border-radius: 60px;
     cursor: pointer;
     background: #eee;
+    .button-reflection{
+      width: 100%;
+      height: 100px;
+      object-fit: cover;
+      opacity: 0.7;
+      filter:blur(3px) saturate(0.4) brightness(1.1);//模糊度
+      //filter:saturate(0.4)//对比度
+      //filter:brightness(1.1);//亮度
+    }
   }
   
   .button.pressed {
